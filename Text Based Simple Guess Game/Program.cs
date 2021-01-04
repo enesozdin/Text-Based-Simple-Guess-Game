b6cb9b9ec1f87724ff89ef694ea762f9107dc51e
enesozdin <52005217+enesozdin@users.noreply.github.com>
2021-01-04 18:43:04 +0300

    Add project files.

---
+++ Text Based Simple Guess Game/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Text_Based_Simple_Guess_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zorluğu Seçiniz:\nKolay\nNormal\nZor");
+            string secim = Console.ReadLine().ToLower();
+            if (secim == "kolay")
+            {
+                Console.WriteLine("Kolay modunu seçtiniz"); Console.WriteLine("Sayıyı tahmin ediniz");
+            }
+            else if (secim == "normal")
+            {
+                Console.WriteLine("normal modunu seçtiniz"); Console.WriteLine("Sayıyı tahmin ediniz");
+            }
+            else if (secim == "zor")
+            {
+                Console.WriteLine("zor seçtiniz"); Console.WriteLine("Sayıyı tahmin ediniz");
+            }
+            else
+            {
+                Console.WriteLine("kolay,normal,zor seçmelisiniz!");
+            }
+            if (secim == "kolay")
+            {
+                Random rnd = new Random();
+                int hedef = rnd.Next(10);
+                Console.WriteLine(hedef);
+                for (int i = 10; i <= 10; i--)
+                {
+                    Console.WriteLine("Kalan hakkınız:");
+                    Console.WriteLine(i);
+                    int tahmin = int.Parse(Console.ReadLine());
+                    if (tahmin == hedef)
+                    {
+                        Console.WriteLine("Tebrikler kazandınız!\nHerhangi bir tuşa basarak çıkabilirsiniz.");
+                        break;
+                    }
+                    else if (i == 0 && tahmin != hedef)
+                    {
+                        Console.WriteLine("Oyunu kaybettiniz lütfen tekrar başlatınız!");
+                        break;
+                    }
+                }
+            }
+            if (secim == "normal")
+            {
+                Random rnd = new Random();
+                int hedef = rnd.Next(25);
+                Console.WriteLine(hedef);
+                for (int i = 5; i <= 5; i--)
+                {
+                    Console.WriteLine("Kalan Hakkınız");
+                    Console.WriteLine(i);
+                    int tahmin = int.Parse(Console.ReadLine());
+                    Console.WriteLine(tahmin == hedef ? "Tahmininiz doğru!" : "Tekrar deneyiniz!");
+                    if (tahmin == hedef)
+                    {
+                        Console.WriteLine("Tebrikler kazandınız!\nHerhangi bir tuşa basarak çıkabilirsiniz.");
+                        break;
+                    }
+                    else if (i == 0 && tahmin != hedef)
+                    {
+                        Console.WriteLine("Oyunu kaybettiniz lütfen tekrar başlatınız!");
+                        break;
+                    }
+                }
+            }
+            if (secim == "zor")
+            {
+                Random rnd = new Random();
+                int hedef = rnd.Next(50);
+                Console.WriteLine(hedef);
+                for (int i = 3; i < 4; i--)
+                {
+                    Console.WriteLine("Kalan Hakkınız");
+                    Console.WriteLine(i);
+                    int tahmin = int.Parse(Console.ReadLine());
+                    if (tahmin == hedef)
+                    {
+                        Console.WriteLine("Tebrikler kazandınız!\nHerhangi bir tuşa basarak çıkabilirsiniz.");
+                        break;
+                    }
+                    else if (i == 0 && tahmin != hedef)
+                    {
+                        Console.WriteLine("Oyunu kaybettiniz lütfen tekrar başlatınız!");
+                        break;
+                    }
+
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+    
+}
---
+++ Text Based Simple Guess Game/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Text Based Simple Guess Game")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Text Based Simple Guess Game")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8ed58c14-49fc-4365-ba60-c5c535cb57b6")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
